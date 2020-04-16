b3e47859d2c271ef43ea97ee3a2b20911dc44c9a
yola014 <yolasbrnsa@gmail.com>
2020-04-16 11:09:54 +0700

    Project Buku Alamat

---
+++ src/app/dialog-konfirmasi/dialog-konfirmasi.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-konfirmasi',
+  templateUrl: './dialog-konfirmasi.component.html',
+  styleUrls: ['./dialog-konfirmasi.component.scss']
+})
+export class DialogKonfirmasiComponent implements OnInit {
+
+  constructor(public dialogRef:MatDialogRef<DialogKonfirmasiComponent>) { }
+  konfirmasi()
+  {
+    	this.dialogRef.close(true);
+  }
+  ngOnInit(): void {
+  }
+
+}
